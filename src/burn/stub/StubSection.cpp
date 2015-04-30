cb925a7d0f63432276d6d930f80892cddbc3f83f
Bob Arnson <bob@joyofsetup.com>
2015-04-30 00:36:58 -0400

    Try a workaround to fix Dev14 CTP6/RC failure to handle #pragma data_seg

--- src/burn/stub/StubSection.cpp
+++ src/burn/stub/StubSection.cpp
@@ -12,19 +12,18 @@
 #pragma section(".wixburn",read)
 
 // If these defaults ever change, be sure to update constants in burn\engine\section.cpp as well.
-#pragma data_seg(push, ".wixburn")
-static DWORD dwMagic = 0x00f14300;
-static DWORD dwVersion = 0x00000002;
+__declspec(allocate(".wixburn")) static DWORD dwMagic = 0x00f14300;
+__declspec(allocate(".wixburn")) static DWORD dwVersion = 0x00000002;
 
-static GUID guidBundleId = { };
+__declspec(allocate(".wixburn")) static GUID guidBundleId = { };
 
-static DWORD dwStubSize = 0;
-static DWORD dwOriginalChecksum = 0;
-static DWORD dwOriginalSignatureOffset = 0;
-static DWORD dwOriginalSignatureSize = 0;
+__declspec(allocate(".wixburn")) static DWORD dwStubSize = 0;
+__declspec(allocate(".wixburn")) static DWORD dwOriginalChecksum = 0;
+__declspec(allocate(".wixburn")) static DWORD dwOriginalSignatureOffset = 0;
+__declspec(allocate(".wixburn")) static DWORD dwOriginalSignatureSize = 0;
 
-static DWORD dwContainerFormat = 1;
-static DWORD dwContainerCount = 0;
-static DWORD qwBootstrapperApplicationContainerSize = 0;
-static DWORD qwAttachedContainerSize = 0;
-#pragma data_seg(pop)
+__declspec(allocate(".wixburn")) static DWORD dwContainerFormat = 1;
+__declspec(allocate(".wixburn")) static DWORD dwContainerCount = 0;
+__declspec(allocate(".wixburn")) static DWORD qwBootstrapperApplicationContainerSize = 0;
+__declspec(allocate(".wixburn")) static DWORD qwAttachedContainerSize = 0;
+
